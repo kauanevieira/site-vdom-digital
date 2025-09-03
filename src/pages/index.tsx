0013fa0ac5c2c501feeb4a00f8dd02e619e67244
Kauane Vieira <kauaneviieira@gmail.com>
2025-09-03 18:05:13 -0300

    feat: add foundational components and styles for the technology institutional website

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Footer, Header, Hero } from '@/components';
+
+export default function HomePage() {
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <Footer />
+        </div>
+    );
+};
---
+++ src/pages/services.tsx
@@ -0,0 +1,21 @@
+import { Footer, Header } from "@/components";
+
+export default function Services() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h1>Nossos Serviços</h1>
+                <p>Oferecemos uma variedade de serviços tecnológicos para atender às suas necessidades.</p>
+                <ul>
+                    <li>Desenvolvimento de Software Personalizado</li>
+                    <li>Consultoria em TI</li>
+                    <li>Suporte Técnico</li>
+                    <li>Integração de Sistemas</li>
+                    <li>Treinamento e Capacitação</li>
+                </ul>
+            </main>
+            <Footer />
+        </div>
+    );
+};
